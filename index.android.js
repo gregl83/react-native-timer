7712d0f5dbfdf263fef015e05a042b91a58770e5
Gregory Langlais <general@gregorylanglais.com>
2017-12-04 08:35:48 -0500

    add interface to count ticks from background timer

--- index.android.js
+++ index.android.js
@@ -6,19 +6,37 @@ import {
   View
 } from 'react-native';
 
+import Timer from './src/Timer'
+import config from './config.json'
+
 export default class ReactNativeTimer extends Component {
   constructor (props) {
     super(props)
+
+    this.state = {
+        elapsed: 0,
+    }
+
+    let timer = new Timer(config)
+
+    timer.addListener('ticked', () => {
+      this.setState((prevState, props) => ({
+          elapsed: prevState.elapsed + 1
+      }));
+    })
+
+    timer.start()
+
     // todo - this.handler.bind(this) events
   }
   componentDidMount() {}
   componentWillUnmount() {}
   render() {
+    let elapsed = this.state.elapsed
     return (
       <View style={styles.container}>
-        <Text style={styles.header}>
-          React Native Timer
-        </Text>
+        <Text style={styles.header}>React Native Timer</Text>
+        <Text style={styles.header}>Ticks: {elapsed}</Text>
       </View>
     );
   }
